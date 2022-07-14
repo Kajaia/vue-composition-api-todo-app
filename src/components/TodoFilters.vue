ed73a680becc4d2636feb3781fc9a0493a955b7d
Lasha Kajaia <lashakajaia31@gmail.com>
2022-07-15 01:32:06 +0400

    Added warning notification for filters

--- src/components/TodoFilters.vue
+++ src/components/TodoFilters.vue
@@ -1,7 +1,21 @@
+<script setup>
+import toast from "../alerts/toast";
+
+const soon = () => {
+  toast("warning", "Working on it, be patient!");
+};
+</script>
+
 <template>
   <div class="d-flex align-items-center gap-2">
-    <button class="btn btn-sm btn-primary shadow-sm px-3">All</button>
-    <button class="btn btn-sm btn-light shadow-sm px-3">Active</button>
-    <button class="btn btn-sm btn-light shadow-sm px-3">Completed</button>
+    <button @click.stop="soon" class="btn btn-sm btn-primary shadow-sm px-3">
+      All
+    </button>
+    <button @click.stop="soon" class="btn btn-sm btn-light shadow-sm px-3">
+      Active
+    </button>
+    <button @click.stop="soon" class="btn btn-sm btn-light shadow-sm px-3">
+      Completed
+    </button>
   </div>
 </template>
