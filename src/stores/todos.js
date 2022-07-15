4ee30b839b539a68adf38f23ebc3bb16118d08c9
Kajaia <lashakajaia31@gmail.com>
2022-07-15 17:40:15 +0400

    Added localstorage for todos

--- src/stores/todos.js
+++ src/stores/todos.js
@@ -1,13 +1,19 @@
 import { defineStore } from "pinia";
 
+const updateLocalStorage = (todos) => {
+  localStorage.setItem("todos", JSON.stringify(todos));
+};
+
 export const useTodos = defineStore({
   id: "todos",
   state: () => ({
-    todos: [
-      { id: 1, title: "Lorem ipsum", status: false },
-      { id: 2, title: "Dolore ipsem", status: true },
-      { id: 3, title: "Sit amet", status: false },
-    ],
+    todos: localStorage.todos
+      ? JSON.parse(localStorage.todos)
+      : [
+          { id: 1, title: "Lorem ipsum", status: false },
+          { id: 2, title: "Dolore ipsem", status: true },
+          { id: 3, title: "Sit amet", status: false },
+        ],
   }),
   actions: {
     addTodo(title) {
@@ -16,9 +22,13 @@ export const useTodos = defineStore({
         title: title,
         status: false,
       });
+
+      updateLocalStorage(this.todos);
     },
     deleteTodo(id) {
       this.todos = this.todos.filter((todo) => todo.id !== id);
+
+      updateLocalStorage(this.todos);
     },
     checkTodo(id) {
       const todo = this.todos.find((todo) => todo.id === id);
@@ -26,9 +36,13 @@ export const useTodos = defineStore({
       if (todo) {
         todo.status = !todo.status;
       }
+
+      updateLocalStorage(this.todos);
     },
     clearCompleted() {
       this.todos = this.todos.filter((todo) => todo.status !== true);
+
+      updateLocalStorage(this.todos);
     },
     updateTodoTitle(obj, str) {
       const todo = this.todos.find((todo) => todo.id === obj.id);
@@ -36,6 +50,8 @@ export const useTodos = defineStore({
       if (todo) {
         todo.title = str;
       }
+
+      updateLocalStorage(this.todos);
     },
   },
 });
